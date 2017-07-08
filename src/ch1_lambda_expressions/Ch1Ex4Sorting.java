4a304a912406ca9e409505a3413b4b9f8e94ab25
lesiak <lesiak@outlook.com>
2017-07-08 22:19:25 +0200

    Imprved sorting

--- src/ch1_lambda_expressions/Ch1Ex4Sorting.java
+++ src/ch1_lambda_expressions/Ch1Ex4Sorting.java
@@ -2,6 +2,7 @@ package ch1_lambda_expressions;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 /**
@@ -12,13 +13,7 @@ import java.util.stream.Stream;
 public class Ch1Ex4Sorting {
     public static void main(String[] args) {
         File[] files = getFilesToProcess();
-        Arrays.sort(files, (f1, f2) -> {
-            int dirCompare = compareIsDir(f1, f2);
-            if (dirCompare!=0) {
-                return dirCompare;
-            }
-            return f1.getAbsolutePath().compareTo(f2.getAbsolutePath());
-        });
+        sortVersion2(files);
         Arrays.stream(files).map(File::getAbsolutePath).forEach(System.out::println);
     }
 
@@ -27,6 +22,29 @@ public class Ch1Ex4Sorting {
         File[] localFiles = f.listFiles();
         File[] roots = File.listRoots();
         return Stream.concat(Arrays.stream(localFiles), Arrays.stream(roots)).toArray(File[]::new);
+    }
+
+    private static void sortVersion1(File[] files) {
+        Arrays.sort(files, (f1, f2) -> {
+            int dirCompare = compareIsDir(f1, f2);
+            if (dirCompare!=0) {
+                return dirCompare;
+            }
+            return f1.getAbsolutePath().compareTo(f2.getAbsolutePath());
+        });
+    }
+
+    private static void sortVersion2(File[] files) {
+        Arrays.sort(files,
+                Comparator.comparing(
+                        Ch1Ex4Sorting::FileDir0NonDir1)
+                .thenComparing(
+                        File::getAbsolutePath));
+
+    }
+
+    static int FileDir0NonDir1(File f) {
+        return f.isDirectory() ? 0 : 1;
     }
 
     static int compareIsDir(File f1, File f2) {
