a666860976ef94817de66b01d5491283260eac60
lesiak <lesiak@outlook.com>
2017-07-13 14:10:18 +0200

    Ch2 Ex1 Fork join

--- src/main/java/ch2_stream_api/Ch2Ex1ManualParallel.java
+++ src/main/java/ch2_stream_api/Ch2Ex1ManualParallel.java
@@ -10,6 +10,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -27,15 +29,15 @@ public class Ch2Ex1ManualParallel {
         String contents = new String(Files.readAllBytes(alicePath), StandardCharsets.UTF_8); // Read ﬁle into string
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        //countManualLoop(words);
-        //countStreams(words);
-        //countStreamsParallel(words);
+        countManualLoop(words);
+        countStreams(words);
+        countStreamsParallel(words);
 
 
         int cores = Runtime.getRuntime().availableProcessors();
         System.out.println("cores:" + cores);
         countManualParallelLoop(words, cores);
-
+        countForkJoin(words);
     }
 
     private static void countManualLoop(List<String> words) {
@@ -45,23 +47,23 @@ public class Ch2Ex1ManualParallel {
                 count++;
             }
         }
-        System.out.println(count);
+        System.out.println("Manual loop:" + count);
     }
 
     private static void countStreams(List<String> words) {
         long count = words.stream().filter(w -> w.length() > 12).count();
-        System.out.println(count);
+        System.out.println("Streams:" + count);
     }
 
     private static void countStreamsParallel(List<String> words) {
         long count = words.parallelStream().filter(w -> w.length() > 12).count();
-        System.out.println(count);
+        System.out.println("Parallel Streams:" + count);
     }
 
     private static void countManualParallelLoop(List<String> words, int cores) throws InterruptedException {
-        List<List<String>> wordsBatches = splitIntoEqualParts(words,cores);
+        List<List<String>> wordsBatches = splitIntoEqualParts(words, cores);
 
-       int[] counts = new int[wordsBatches.size()];
+        int[] counts = new int[wordsBatches.size()];
         List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < wordsBatches.size(); ++i) {
             int idx = i;
@@ -73,24 +75,64 @@ public class Ch2Ex1ManualParallel {
                         count++;
                     }
                 }
-                counts[idx] =  count;
+                counts[idx] = count;
             };
             Thread t = new Thread(r);
             t.start();
             threads.add(t);
         }
 
-        for (Thread t : threads)
-        {
+        for (Thread t : threads) {
             t.join();
         }
         int count = IntStream.of(counts).sum();
-        System.out.println(count);
+        System.out.println("Manual parallel result: " + count);
     }
 
-    private static <E> List<List<E>> splitIntoEqualParts(List<E> list, int numGroups){
-        final int groupSize = (list.size() + numGroups - 1 )/ numGroups;
-        
+    private static void countForkJoin(List<String> words) {
+        ForkJoinPool pool = ForkJoinPool.commonPool();
+        System.out.println("Pool parallelism:" + pool.getParallelism());
+        Integer count = pool.invoke(new CountLongWordsTask(words));
+        System.out.println("Fork-join result: " + count);
+
+    }
+
+    static class CountLongWordsTask extends RecursiveTask<Integer> {
+        protected static int sThreshold = 100;
+        private final List<String> words;
+
+        public CountLongWordsTask(List<String> words) {
+            this.words = words;
+        }
+
+        @Override
+        protected Integer compute() {
+            if (words.size() < sThreshold) {
+                return computeDirectly();
+            }
+            int split = words.size() / 2;
+            CountLongWordsTask t1 = new CountLongWordsTask(words.subList(0, split));
+            CountLongWordsTask t2 = new CountLongWordsTask(words.subList(split, words.size()));
+            t1.compute();
+            t1.fork();
+            t2.fork();
+            return t2.join() + t1.join();
+        }
+
+        protected Integer computeDirectly() {
+            int count = 0;
+            for (String w : words) {
+                if (w.length() > 12) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    private static <E> List<List<E>> splitIntoEqualParts(List<E> list, int numGroups) {
+        final int groupSize = (list.size() + numGroups - 1) / numGroups;
+
         // old style
         //List<List<E>> result = new ArrayList<>(numGroups);
         //IntStream.range(0, list.size())
